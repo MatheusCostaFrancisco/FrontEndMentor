99312289868e16864ba839cd8bdaab13b685d997
MatheusCostaFrancisco <contato.matheuscf@gmail.com>
2023-01-11 14:29:54 -0300

    :art: Finish project 2

--- 02-Qr-code-component-main/src/App.vue
+++ 02-Qr-code-component-main/src/App.vue
@@ -1,29 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import QRCodeCard from './components/QRCodeCard.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <QRCodeCard
+      title="Improve your front-end skills by building projects"
+      msg="  Scan the QR code to visit Frontend Mentor and take your coding skills to
+      the next level"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+  }
 </style>
---
+++ 02-Qr-code-component-main/src/components/QRCodeCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <div class="card__img"></div>
+    <div class="card__content">
+      <h2 class="card__title">
+        {{ title }}
+      </h2>
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  defineProps<{ msg: string; title: string }>()
+
+  const count = ref(0)
+</script>
+
+<style scoped>
+  .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 20rem;
+    height: 33rem;
+    background-color: var(--white);
+    border-radius: 0.5rem 0.5rem 1rem 1rem;
+    padding: 1rem;
+    text-align: center;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  }
+  .card__img {
+    background: url('../assets/image-qr-code.png') no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 20rem;
+    border-radius: 0.5rem;
+  }
+
+  .card__content {
+    padding: 1rem;
+  }
+
+  .card__title {
+    font-weight: 700;
+    color: var(--dark-blue);
+    font-size: 1.5rem;
+  }
+
+  p {
+    color: var(--grayish-blue);
+  }
+</style>
