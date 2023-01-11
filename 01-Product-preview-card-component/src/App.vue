b42774327babac18eda151399fc0cbfd008b4209
MatheusCostaFrancisco <contato.matheuscf@gmail.com>
2023-01-11 12:23:04 -0300

    :tada: fix project 01

---
+++ 01-Product-preview-card-component/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="content" role="main">
+    <CardProductComponent />
+  </main>
+  <div class="attribution" role="contentinfo">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by
+    <a target="_blank" href="https://github.com/MatheusCostaFrancisco"
+      >Matheus Costa Francisco</a
+    >.
+  </div>
+</template>
+
+<script lang="ts">
+  import CardProductComponent from './components/CardProductComponent.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      CardProductComponent,
+    },
+  }
+</script>
+
+<style scoped>
+  .content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 90%;
+    width: 100%;
+  }
+
+  .attribution {
+    text-align: center;
+  }
+</style>
