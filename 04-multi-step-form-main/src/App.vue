b883fd206936e4098e5fd398727785b235edc4c7
MatheusCostaFrancisco <contato.matheuscf@gmail.com>
2023-01-15 01:06:05 -0300

    :tada: first commit to 04-projetc

---
+++ 04-multi-step-form-main/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <div class="steps-section">
+      <Steps :steps="steps" />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Steps from './components/Steps.vue'
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      Steps,
+    },
+    data: () => ({
+      steps: ['YOUR INFO', 'SELECT PLAN', 'ADD INFO', 'SUMARRY'],
+    }),
+  })
+</script>
+
+<style scoped>
+  main {
+    display: flex;
+    align-items: center;
+    width: 62rem;
+    min-height: 40rem;
+    background-color: aqua;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 1rem;
+    padding: 1rem;
+  }
+
+  .steps-section {
+    background-image: url('./assets/images/bg-sidebar-desktop.svg');
+    width: 17.13rem;
+    height: 35.5rem;
+    padding: 1rem;
+  }
+</style>
---
+++ 04-multi-step-form-main/src/components/Steps.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav name="navidagarion-steps" role="nav">
+    <ul class="steps-list">
+      <li v-for="(step, index) in steps" class="step-item">
+        <div class="step-item__icon">{{ index + 1 }}</div>
+        <div class="step-item__title">
+          <span>Step {{ index }}</span>
+          <h1>{{ step }}</h1>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+  import { PropType } from '@vue/runtime-core'
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'StepsComponent',
+    props: {
+      steps: {
+        type: Array as PropType<String[]>,
+      },
+    },
+  })
+</script>
+
+<style scoped>
+  .steps-list {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+  .step-item {
+    display: flex;
+    gap: 1rem;
+  }
+  .step-item__icon {
+    width: 3rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 1px solid var(--pastel-blue);
+  }
+  .step-item__icon:hover {
+    background-color: var(--pastel-blue);
+    cursor: pointer;
+    color: var(--marine-blue);
+  }
+  .step-item__title {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
